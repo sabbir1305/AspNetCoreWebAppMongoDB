9d8b617dca919d781f3101027e5c6601c09580d3
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-03-30 11:57:53 +0600

    Add project files.

---
+++ CarGalleryApp/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarGalleryApp.Models
+{
+    public class Car
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Brand")]
+        [Required]
+        public string Brand { get; set; }
+
+
+    }
+}
